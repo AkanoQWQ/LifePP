5831ca6b446b33f64725190b726197ded514db22
Akano <61079646+AkanoQWQ@users.noreply.github.com>
2026-01-28 01:10:17 +0800

    Update comp.sh to add DEBUG file

--- comp.sh
+++ comp.sh
@@ -1,3 +1,4 @@
 mkdir -p build
 g++ smana/smana.cpp -o build/smana -mconsole -municode -Icommon -Itools
-g++ server/lifepp-server.cpp -o build/lifepp-server -mconsole -municode -Icommon -Itools
+g++ server/lifepp-server.cpp -o build/lifepp-server -mwindows -municode -Icommon -Itools
+g++ server/lifepp-server.cpp -o build/lifepp-server-DEBUG -mconsole -municode -Icommon -Itools
