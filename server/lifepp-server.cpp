098011a99e355ce89afd44641346cc6e8809a9f6
Akano <61079646+AkanoQWQ@users.noreply.github.com>
2026-01-28 01:12:10 +0800

    make server read from config.json

--- server/lifepp-server.cpp
+++ server/lifepp-server.cpp
@@ -8,17 +8,20 @@
 #include <fcntl.h>
 #include <locale>
 #include <format>
+#include <fstream>
 
+#include "nlohmannJson.hpp"
 #include "smana.hpp"
 #include "NamedPipe.hpp"
+#include "lifeppTools.hpp"
+
+using Json = nlohmann::json;
 
 NamedPipe::NamedPipeServer<Smana::SmanaMessage,Smana::SmanaResponse> smanaServer(Smana::smanaPipeName);
 Smana::Clock smanaClock;
-Smana::SecondType restTime = 60 * 5;
-Smana::SecondType alarmStart = 60 * 40;
-Smana::SecondType alarmEnd = 60 * 60;
-Smana::SecondType AlarmInterval1 = 60 * 5;
-Smana::SecondType AlarmInterval2 = 10;
+
+Smana::SecondType restTime,alarmStart,alarmEnd;
+Smana::SecondType alarmIntervalSlow,alarmIntervalQuick;
 
 auto smanaCallback = [](const Smana::SmanaMessage& msg) -> Smana::SmanaResponse{
     Smana::SmanaResponse ret;
@@ -35,11 +38,50 @@ auto smanaCallback = [](const Smana::SmanaMessage& msg) -> Smana::SmanaResponse{
     return ret;
 };
 
+Json ReadConfig(){
+    auto/*opt<path>*/ appdata = lifepp::tools::GetEnv("APPDATA");
+    if(appdata.has_value() == false){
+        std::wcout<<L"没有找到APPDATA!"<<std::endl;
+        return Json::object();
+    }
+    std::filesystem::path configFile = appdata.value() / "lifepp" / "config.json";
+    std::ifstream instream(configFile);
+    if(!instream.is_open()){
+        std::wcout<<L"configFile无法打开!"<<std::endl;
+        return Json::object();
+    }
+    Json json;
+    instream>>json;
+    return json;
+}
+void SmanaConfig(const Json& json){
+    restTime = json.value("restTime",Smana::defaultRestTime);
+    alarmStart = json.value("alarmStart",Smana::defaultAlarmStart);
+    alarmEnd = json.value("alarmEnd",Smana::defaultAlarmEnd);
+    alarmIntervalSlow = json.value("alarmIntervalSlow",Smana::defaultAlarmIntervalSlow);
+    alarmIntervalQuick = json.value("alarmIntervalQuick",Smana::defaultAlarmIntervalQuick);
+    smanaClock.SetClock(alarmStart,alarmEnd,alarmIntervalSlow,alarmIntervalQuick);
+    smanaClock.SetStartTime(Smana::GetSecond());
+    std::wcout<<L"read smana config : \n";
+    std::wcout<<L"restTime : "<<restTime<<L"\n";
+    std::wcout<<L"alarmStart : "<<alarmStart<<L"\n";
+    std::wcout<<L"alarmEnd : "<<alarmEnd<<L"\n";
+    std::wcout<<L"alarmIntervalSlow : "<<alarmIntervalSlow<<L"\n";
+    std::wcout<<L"alarmIntervalQuick : "<<alarmIntervalQuick<<std::endl;
+    return ;
+}
+
 int wmain(int argc, wchar_t* argv[]){
     _setmode(_fileno(stdout),_O_U16TEXT);
-    std::wcout.imbue(std::locale(""));
-    smanaClock.SetClock(alarmStart,alarmEnd,AlarmInterval1,AlarmInterval2);
-    smanaClock.SetStartTime(Smana::GetSecond());
+    try{
+        std::wcout.imbue(std::locale("C.UTF-8"));
+    }catch(const std::runtime_error&){
+        std::wcout.imbue(std::locale::classic());
+    }
+
+    Json config = ReadConfig();
+    SmanaConfig(config);
+
     smanaServer.SetHandler(smanaCallback);
     smanaServer.Start();
     while(true){
